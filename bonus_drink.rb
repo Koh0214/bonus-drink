e569cad834fa405ca4cc0d343699bf5f7838eadb
Kohei Kishimoto <kkishimotok@gmail.com>
2019-03-01 14:04:34 +0900

    add method(total_count_for)

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,19 @@
 class BonusDrink
+  puts "最初に購入した飲み物の本数を入力してください"
+  amount = gets.to_i
+  
   def self.total_count_for(amount)
-    0
+    total_count = 0
+
+    while amount >= 3 do
+        # 飲んだ本数を加算
+        total_count = total_count + (amount/3) * 3
+        # ボーナスでもらえる数と、余りを合計して次のループに回す
+        amount = (amount/3) + (amount%3)
+    end
+
+    puts "飲んだドリンクは #{amount + total_count} 本"
   end
+
+  print total_count_for(amount)
 end
